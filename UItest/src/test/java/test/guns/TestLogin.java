17db2e87e9d3296bd635043689bab244b3671cfb
RandomRole <1169013843@qq.com>
2018-05-31 14:04:19 +0800

    added

---
+++ UItest/src/test/java/test/guns/TestLogin.java
@@ -0,0 +1,20 @@
+package test.guns;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestLogin {
+
+	@BeforeClass
+	public void setUp() {
+		
+	}
+	@Test
+	public void test01() {
+		System.out.println("hello world");
+	}
+	
+	
+
+}
